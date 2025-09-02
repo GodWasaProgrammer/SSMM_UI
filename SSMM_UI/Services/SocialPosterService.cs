35ab73cc7f56f474ee75c4b913bee12a5db02065
GodWasaProgrammer <72033235+GodWasaProgrammer@users.noreply.github.com>
2025-09-02 14:19:36 +0200

    Added SocialPoster proj to main proj for easier mantaining

---
+++ SSMM_UI/Services/SocialPosterService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Poster;
+
+
+namespace SSMM_UI.Services;
+
+public class SocialPosterService
+{
+    public static async Task Discord()
+    {
+        await SocialPoster.RunPoster(DiscordPost:true);
+    }
+
+    public static async Task Facebook()
+    {
+        await SocialPoster.RunPoster(FBpost:true);
+    }
+
+    public static async Task X()
+    {
+        await SocialPoster.RunPoster(XPost:true);
+    }
+}
