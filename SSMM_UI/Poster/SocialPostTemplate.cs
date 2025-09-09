183d60fda0545d77595be1605ee33108dee0e4de
GodWasaProgrammer <bwnils@gmail.com>
2025-09-09 12:32:06 +0200

    General layout of future feature of template posting to social media

---
+++ SSMM_UI/Poster/SocialPostTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SSMM_UI.Poster;
+
+public class SocialPostTemplate
+{
+    private string prefix = string.Empty;
+    private string suffix = string.Empty;
+    private List<string> LinksToStreams = new List<string>();
+    private string isLive = "Is Now LIVE";
+
+    private List<string> Platforms = new List<string>();
+
+    private void BuildPost()
+    {
+        string.Concat(prefix, LinksToStreams, suffix);
+    }
+
+    private void AddPlatform(string platform)
+    {
+        string add = $"On:{platform}";
+        Platforms.Add(add);
+    }
+}
