9d5543addce50adf0ef0b0f551fadd68d5bf29ae
GodWasaProgrammer <bwnils@gmail.com>
2025-09-11 13:37:06 +0200

    created class to handle logic for if selected/authed as regards to the socialposter

---
+++ SSMM_UI/Poster/PostMaster.cs
@@ -0,0 +1,14 @@
+﻿using SSMM_UI.Oauth.Google;
+using SSMM_UI.Oauth.Kick;
+using SSMM_UI.RTMP;
+using System.Collections.Generic;
+
+namespace SSMM_UI.Poster;
+
+public class PostMaster
+{
+    private List<SelectedService>? _selectedServices;
+    private KickAuthResult? _kickauth;
+    private TwitchTokenResponse? _twitchauth;
+    private GoogleOauthResult? _oauthResult;
+}
