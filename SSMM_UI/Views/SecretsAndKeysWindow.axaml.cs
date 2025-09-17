b8731206417b0b12583e28c7ea71e91bf3f1f1aa
GodWasaProgrammer <72033235+GodWasaProgrammer@users.noreply.github.com>
2025-09-17 23:04:25 +0200

    created general layout of SecretsAndKeysWindow

---
+++ SSMM_UI/Views/SecretsAndKeysWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SSMM_UI.Views;
+
+public partial class SecretsAndKeysWindow : UserControl
+{
+    public SecretsAndKeysWindow()
+    {
+        InitializeComponent();
+    }
+}
