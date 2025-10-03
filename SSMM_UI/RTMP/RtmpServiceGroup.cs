f4f374e39f1716cc71b178aefa8364a5cd9ba284
GodWasaProgrammer <72033235+GodWasaProgrammer@users.noreply.github.com>
2025-10-04 00:39:04 +0200

    Added save logic to the SelectedServiceView and VM

--- SSMM_UI/RTMP/RtmpServiceGroup.cs
+++ SSMM_UI/RTMP/RtmpServiceGroup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SSMM_UI.RTMP;
 
@@ -7,4 +8,15 @@ public class RtmpServiceGroup
     public string ServiceName { get; set; } = string.Empty;
     public List<RtmpServerInfo> Servers { get; set; } = [];
     public RecommendedSettings? RecommendedSettings { get; set; }
+
+    public RtmpServiceGroup Clone()
+    {
+        return new RtmpServiceGroup
+        {
+            ServiceName = ServiceName,
+            Servers = this.Servers.Select(s => new RtmpServerInfo { ServerName = s.ServerName, ServiceName = s.ServiceName, Url = s.Url }).ToList(),
+            RecommendedSettings = RecommendedSettings
+            
+        };
+    }
 }
--- SSMM_UI/ViewModel/SelectedServiceViewModel.cs
+++ SSMM_UI/ViewModel/SelectedServiceViewModel.cs
@@ -1,14 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SSMM_UI.RTMP;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SSMM_UI.ViewModel;
 
 public partial class SelectedServiceViewModel : ObservableObject
 {
-    [ObservableProperty] SelectedService viewedService;
+    private readonly SelectedService _original;
+    [ObservableProperty] SelectedService? viewedService;
+    [ObservableProperty] string? _displayName;
+    [ObservableProperty] string? _streamKey;
+    [ObservableProperty] RtmpServiceGroup? _serviceGroup;
 
     public SelectedServiceViewModel(SelectedService selection)
     {
-        viewedService = selection;
+        _original = selection;
+
+        if(_original != null)
+        {
+            DisplayName = selection.DisplayName;
+            StreamKey = selection.StreamKey;
+
+            _serviceGroup = selection.ServiceGroup?.Clone();
+        }
+        SaveCMD = new RelayCommand(Save);
+    }
+
+    public ICommand SaveCMD { get; }
+
+    public void Save()
+    {
+        _original.StreamKey = StreamKey;
+    }
+
+    public void Cancel()
+    {
+
     }
 }
