61c7736f6f6ec4d91dfd157d25b324b009ae6871
GodWasaProgrammer <bwnils@gmail.com>
2025-10-04 13:58:09 +0200

    Added button hide/show servers and logic for saving the server changes

--- SSMM_UI/ViewModel/SelectedServiceViewModel.cs
+++ SSMM_UI/ViewModel/SelectedServiceViewModel.cs
@@ -9,11 +9,13 @@ namespace SSMM_UI.ViewModel;
 
 public partial class SelectedServiceViewModel : ObservableObject
 {
-    private readonly SelectedService _original;
+    private readonly SelectedService? _original;
     [ObservableProperty] SelectedService? viewedService;
     [ObservableProperty] string? _displayName;
     [ObservableProperty] string? _streamKey;
     [ObservableProperty] RtmpServiceGroup? _serviceGroup;
+    [ObservableProperty] RtmpServerInfo? _selectedServer;
+    [ObservableProperty] bool _showServerList = false;
 
     public SelectedServiceViewModel(SelectedService selection)
     {
@@ -27,13 +29,17 @@ public partial class SelectedServiceViewModel : ObservableObject
             _serviceGroup = selection.ServiceGroup?.Clone();
         }
         SaveCMD = new RelayCommand(Save);
+        ShowServers = new RelayCommand(() => ShowServerList = !ShowServerList);
     }
 
+    // commands
     public ICommand SaveCMD { get; }
-
+    public ICommand ShowServers { get; }
     public void Save()
     {
+        if(_original == null) return;
         _original.StreamKey = StreamKey;
+        _original.SelectedServer = SelectedServer;
     }
 
     public void Cancel()
